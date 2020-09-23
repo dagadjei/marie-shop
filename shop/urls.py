f795215972c2feee5bf08d53277c2f0e98fe90cd
dagadjei <adjeidag@gmail.com>
2020-09-23 18:23:05 +0000

    created and edited routes

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import item_list
+from .views import item_list, checkout, product
 
 app_name = 'shop'
 urlpatterns = [
-    path('', item_list, name='item-list')
+    path('', item_list, name='item-list'),
+    path('checkout/', checkout, name='checkout'),
+    path('product/', product, name='product'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -6,3 +6,10 @@ def item_list(request):
         'items': Item.objects.all()
     }
     return render(request, "home-page.html", context)
+
+def checkout(request):
+    return render(request, 'checkout-page.html')
+
+
+def product(request):
+    return render(request, 'product-page.html')
