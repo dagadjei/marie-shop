22bd0e731c5ddb33ffcd695ac634ae917dd01811
dagadjei <adjeidag@gmail.com>
2020-10-07 18:28:32 +0000

    Revert "set static root" yes
    This reverts commit b4da8b220886857bff974d92cf12c09e2b43ffd4.

--- commerce/aws/conf.py
+++ commerce/aws/conf.py
@@ -13,7 +13,6 @@ S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_ROOT = MEDIA_URL
 STATIC_URL = S3_URL + 'static/'
-STATIC_ROOT = STATIC_URL
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
 two_months = datetime.timedelta(days=61)
