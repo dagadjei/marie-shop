074cc3d1d63758fe5987c4cf2da692bb64ba0c67
dagadjei <adjeidag@gmail.com>
2020-10-08 00:25:49 +0000

    lets see

--- commerce/settings.py
+++ commerce/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from commerce.aws.conf import *
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -166,4 +167,4 @@ AWS_DEFAULT_ACL = None
 
 #DEFAULT_FILE_STORAGE = 'commerce.storages.MediaStore'
 """
-
+django_heroku.settings(locals())
